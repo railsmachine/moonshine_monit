43e6c4894a1ea728518635316028108e7f629920
Josh Nichols <josh@railsmachine.com>
2011-07-12 16:13:08 +0000

    Introduce monit role, to avoid restarting on servers its not available.

--- recipes/monit.rb
+++ recipes/monit.rb
@@ -1,22 +1,22 @@
 namespace :monit do
   desc "Stop monit"
-  task :stop do
+  task :stop, :roles => :monit do
     sudo "stop monit"
   end
 
   desc "Start monit"
-  task :start do
+  task :start, :roles => :monit do
     sudo "start monit"
   end
 
   desc "Restart monit"
-  task :restart do
+  task :restart, :roles => :monit do
     sudo "stop monit || true"
     sudo "start monit"
   end
 
   desc "Reload monit config"
-  task :reload do
+  task :reload, :roles => :monit do
     sudo 'monit reload'
   end
 end
